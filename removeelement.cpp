d882955a5a760177f9fbb012dcf2368dbf0868cb
tusharrathod18 <tusharjitendraratho@gmail.com>
2025-07-23 22:21:06 +0530

    Add files via upload

---
+++ removeelement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;  // Count of elements not equal to val
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];  // Move non-val element to the front
+                k++;
+            }
+        }
+        return k;
+    }
+};
